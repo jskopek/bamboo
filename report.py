02065992fefc8da3b70484990b85022d45d7853d
Jean-Marc Skopek <jskopek@gmail.com>
2012-01-18 01:21:08 -0500

    Parse a method path from a filename and method name

--- report.py
+++ report.py
@@ -5,6 +5,7 @@ import hotshot.stats
 import sys
 import re
 
+app_path = "/Users/jms/Documents/Code/Python/bamboo/"
 new_stdout = StringIO.StringIO()
 sys.stdout = new_stdout
 
@@ -17,11 +18,25 @@ print "OUTPUT:"
 printed = new_stdout.getvalue()
 print printed
 
+def parse_method_path(filename, method, app_path):
+    if not app_path in filename:
+        return False
+
+    filename = filename.replace(app_path, '')
+    filename = filename.replace(".py", '')
+
+    path_components = filename.split("/")
+    path_components.append(method)
+    return ".".join(path_components)
+
+
 for line in printed.split("\n"):
     m = re.match("\s+(\d+)\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)\s+([^:]+):(\d+)\(([^)]+)\)$", line)
     if m:
         print "LINE: %s"%line
         ncalls, tottime, percall_other, cumtime, percall, filename, linenum, method = m.groups(0)
+        method_path = parse_method_path(filename, method, app_path)
+        print method_path
         print ncalls
         print tottime
         print percall
@@ -29,6 +44,8 @@ for line in printed.split("\n"):
         print filename
         print linenum
         print method
+
+
 
 #print "--- callers -----"
 #stats.sort_stats("cumulative").print_callers(15)
